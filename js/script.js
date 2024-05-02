d5f3afc613005a48d1b202a48cf26e2c3a968153
SterrenHeer <sterrenheer@gmail.com>
2024-05-02 16:28:53 +0300

    add timer and some content to monthly section

--- js/script.js
+++ js/script.js
@@ -270,6 +270,63 @@ function slider({containerSelector, slideSelector, nextSlideSelector, prevSlideS
     }
 }
 
+function timer(id, deadline) {
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = hours = minutes = seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24));
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+            minutes = Math.floor((t / 1000 / 60) % 60 );
+            seconds = Math.floor((t / 1000) % 60);
+        }
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock(id, deadline)
+}
+
 if (document.querySelector('.categories_tab') != null) {
     tabs('.categories_tab', '.categories_tab_item', '.categories_tab_content', '.categories_tab_header', 'categories_tab_active');
 }
@@ -321,6 +378,10 @@ if (document.querySelector('.monthly_field') != null) {
         rowGap: 24,
         swipe: true,
     });
+}
+
+if (document.querySelector('.monthly_timer') != null) {
+    timer('.monthly_timer', '2024-05-15');
 }
 
 const pageup = document.querySelector('.pageUp');
