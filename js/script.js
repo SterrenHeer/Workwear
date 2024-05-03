b108fc77710fd383c10bbf8fae2c1f1451e06390
SterrenHeer <sterrenheer@gmail.com>
2024-05-03 14:50:18 +0300

    add consult modal window

--- js/script.js
+++ js/script.js
@@ -36,6 +36,40 @@ function tabs(tabsContainerSelector, tabsSelector, tabsContentSelector, tabsPare
 	});
 }
 
+function openModal(modalSelector) {
+    const modal = document.querySelector(modalSelector);
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+}
+
+function closeModal(modalSelector) {
+    const modal = document.querySelector(modalSelector);
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+}
+
+function modal(triggerSelector, closeSelector, modalSelector) {
+    const modalTrigger = document.querySelectorAll(triggerSelector),
+        modal = document.querySelector(modalSelector);
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => openModal(modalSelector));
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal || e.target.getAttribute(closeSelector) == '') {
+            closeModal(modalSelector);
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) { 
+            closeModal(modalSelector);
+        }
+    });
+}
+
 function slider({containerSelector, slideSelector, nextSlideSelector, prevSlideSelector, wrapperSelector, fieldSelector, indicatorsClass, elementsPerPage = 1, elementsPerPageMobile = 1, rowGap = 0, duration = 0, swipe = false}) {
     let slideIndex = 1,
     	offset = 0,
@@ -388,6 +422,10 @@ if (document.querySelector('.monthly_field') != null) {
 
 if (document.querySelector('.monthly_timer') != null) {
     timer('.monthly_timer', '2024-05-15');
+}
+
+if (document.querySelector('.consult') != null) {
+    modal('[data-modal]', 'data-close', '.consult');
 }
 
 const pageup = document.querySelector('.pageUp');
